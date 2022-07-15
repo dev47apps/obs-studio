b22660f720bcdab1966a82cc1274da5d71f94e13
aramg <aramg@users.noreply.github.com>
2022-07-15 16:44:22 -0700

    Update CMakeLists, kill rtmp-services

--- cmake/copy_on_debug_helper.cmake
+++ cmake/copy_on_debug_helper.cmake
@@ -1,7 +1,9 @@
 string(REGEX REPLACE "\\.(dll|exe)$" ".pdb" FNAME "${FNAME}")
 
 if(CONFIG STREQUAL Debug OR CONFIG STREQUAL RelWithDebInfo)
-	file(COPY "${INPUT}/${FNAME}" DESTINATION "${OUTPUT}")
+	if (EXISTS ${INPUT}/${FNAME})
+		file(COPY "${INPUT}/${FNAME}" DESTINATION "${OUTPUT}")
+	endif()
 elseif(EXISTS "${OUTPUT}/${FNAME}")
 	file(REMOVE "${OUTPUT}/${FNAME}")
 endif()
